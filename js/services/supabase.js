a79fe633ff45f361b399212942d612df833aabe0
Floon001 <crazyfloon@hotmail.fr>
2026-05-31 11:24:10 +0200

    Update supabase.js

--- js/services/supabase.js
+++ js/services/supabase.js
@@ -3,7 +3,7 @@ const SUPABASE_URL =
 'https://kecunxfztdnwrkhundhk.supabase.co';
 
 const SUPABASE_PUBLISHABLE_KEY =
-sb_publishable_taPdaYExLU0CS5yu0GvBgw_1_Gc3Cho;
+'sb_publishable_taPdaYExLU0CS5yu0GvBgw_1_Gc3Cho';
 
 export const supabase =
 window.supabase.createClient(
